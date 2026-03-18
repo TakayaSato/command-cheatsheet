bf949b1884f5d19eb2fe2ac38bf9fe4271289eae
佐藤貴哉 <takaya@satoutakashinarinoMacBook-Air.local>
2026-03-18 10:22:07 +0900

    update: githubPagesで表示できる仕様に変更

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/command-cheatsheet/',
   plugins: [react()],
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true,
+  },
 })
